cf2157ad508af087b3bcfd45fb40422031219c85
Jeff Squyres <jsquyres@cisco.com>
2016-09-21 07:53:44 -0700

    Mark this repository as stale.
    The ompi-release repo is dead; long live the ompi repo!
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- ompi/mca/pml/ob1/pml_ob1_rdmafrag.c
+++
@@ -1,9 +0,0 @@
-#include "pml_ob1.h"
-#include "pml_ob1_rdmafrag.h"
-
-
-OBJ_CLASS_INSTANCE(
-    mca_pml_ob1_rdma_frag_t,
-    opal_list_item_t,
-    NULL,
-    NULL);
--- ompi/mca/pml/ob1/post_configure.sh
+++
@@ -1,1 +0,0 @@
-DIRECT_CALL_HEADER="mca/pml/ob1/pml_ob1.h"
